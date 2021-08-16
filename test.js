b979d907d7e98776350d901fbfc7f109d1d8f4c7
FaridFarahmand <faridfarahmandf2f@gmail.com>
2021-08-16 17:34:11 +0430

    introduction TS

---
+++ test.js
@@ -0,0 +1,6 @@
+var x = 10;
+/*when you declare a variable , you can not change the data type when you asign it again*/
+//  x = "Farid"
+/*use this code in cmd for
+compiling everytime you changed sth :
+tsc yourTSfilename.ts -w*/ 
