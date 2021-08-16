5a92d3009d596e04376daee0d1c19aed2ec0a2b0
FaridFarahmand <faridfarahmandf2f@gmail.com>
2021-08-16 21:18:24 +0430

    class part one

--- public/app.js
+++ public/app.js
@@ -1,9 +1,24 @@
 "use strict";
-// const aDOM = document.querySelector('a') ;
-// aDOM?.children
-var classDom = document.querySelector(".a1");
-classDom.href;
-var btn = document.querySelector("#btn");
-btn.addEventListener("click", function (e) {
-    e.preventDefault();
-});
+//class 
+var Person = /** @class */ (function () {
+    function Person(fn, ln, age) {
+        var _this = this;
+        this.information = function () {
+            return "this person is " + _this.firstName + " " + _this.lastName + " , he/she is " + _this.age + " years old ";
+        };
+        this.firstName = fn;
+        this.lastName = ln;
+        this.age = age;
+    }
+    return Person;
+}());
+var person1 = new Person("Farid", "Farahmand", 24);
+var person2 = new Person("Rouzbeh", "Latifi", 24);
+var listOfPerson = [];
+listOfPerson.push(person1, person2);
+console.log(listOfPerson);
+console.log('hi');
+var showPerson = function () {
+    console.log(person1.information());
+};
+showPerson();
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,25 @@
-// const aDOM = document.querySelector('a') ;
-// aDOM?.children
-const classDom = document.querySelector(".a1") as HTMLAnchorElement ;
-classDom.href;
-const btn = document.querySelector("#btn") as HTMLButtonElement ;
-btn.addEventListener("click" , (e:Event) => {
-    e.preventDefault() ;
-}) 
+//class 
+class Person {
+    firstName : string ; 
+    lastName : string ;
+    age : number ;
+    constructor(fn:string , ln:string , age:number) {
+        this.firstName = fn ;
+        this.lastName = ln ;
+        this.age = age ;
+    }
+    information = () => {
+        return `this person is ${this.firstName} ${this.lastName} , he/she is ${this.age} years old `
+    }
+}
+const person1 = new Person("Farid" , "Farahmand" , 24) ;
+const person2 = new Person("Rouzbeh" , "Latifi" , 24) ;
+
+const listOfPerson : Person[] = [] ;
+listOfPerson.push(person1,person2) ;
+console.log(listOfPerson);
+console.log('hi');
+const showPerson = () => {
+    console.log(person1.information()) ;
+}
+showPerson();   
