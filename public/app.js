462f4dd4dba297d3a559a0c9f6daf414509efbc5
FaridFarahmand <faridfarahmandf2f@gmail.com>
2021-08-16 21:25:59 +0430

    class part two

--- public/app.js
+++ public/app.js
@@ -1,24 +1,32 @@
 "use strict";
 //class 
 var Person = /** @class */ (function () {
+    // firstName : string ; 
+    // lastName : string ;
+    // age : number ;
     function Person(fn, ln, age) {
         var _this = this;
-        this.information = function () {
-            return "this person is " + _this.firstName + " " + _this.lastName + " , he/she is " + _this.age + " years old ";
-        };
-        this.firstName = fn;
-        this.lastName = ln;
+        this.fn = fn;
+        this.ln = ln;
         this.age = age;
+        this.information = function () {
+            return "this person is " + _this.fn + " " + _this.ln + " , he/she is " + _this.age + " years old ";
+        };
     }
     return Person;
 }());
 var person1 = new Person("Farid", "Farahmand", 24);
 var person2 = new Person("Rouzbeh", "Latifi", 24);
-var listOfPerson = [];
-listOfPerson.push(person1, person2);
-console.log(listOfPerson);
-console.log('hi');
-var showPerson = function () {
-    console.log(person1.information());
+// const listOfPerson : Person[] = [] ;
+// listOfPerson.push(person1,person2) ;
+// console.log(listOfPerson);
+// console.log('hi');
+// const showPerson = () => {
+//     console.log(person1.information()) ;
+// }
+// showPerson(); 
+var showPersonInformation = function () {
+    console.log(person1.ln + " is " + person1.age + " years old");
+    // return person1.information()
 };
-showPerson();
+console.log(showPersonInformation());
--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,26 @@
 //class 
 class Person {
-    firstName : string ; 
-    lastName : string ;
-    age : number ;
-    constructor(fn:string , ln:string , age:number) {
-        this.firstName = fn ;
-        this.lastName = ln ;
-        this.age = age ;
-    }
-    information = () => {
-        return `this person is ${this.firstName} ${this.lastName} , he/she is ${this.age} years old `
+    // firstName : string ; 
+    // lastName : string ;
+    // age : number ;
+    constructor(private fn:string , public ln:string , readonly age:number) {}
+    private information = () => {
+        return `this person is ${this.fn} ${this.ln} , he/she is ${this.age} years old `
     }
 }
 const person1 = new Person("Farid" , "Farahmand" , 24) ;
 const person2 = new Person("Rouzbeh" , "Latifi" , 24) ;
 
-const listOfPerson : Person[] = [] ;
-listOfPerson.push(person1,person2) ;
-console.log(listOfPerson);
-console.log('hi');
-const showPerson = () => {
-    console.log(person1.information()) ;
-}
-showPerson();   
+// const listOfPerson : Person[] = [] ;
+// listOfPerson.push(person1,person2) ;
+// console.log(listOfPerson);
+// console.log('hi');
+// const showPerson = () => {
+//     console.log(person1.information()) ;
+// }
+// showPerson(); 
+const showPersonInformation = () => {
+    console.log(`${person1.ln} is ${person1.age} years old`)
+    // return person1.information()
+}  
+console.log(showPersonInformation());
